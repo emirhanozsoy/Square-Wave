6a41584c0f91c6360798505a2c57c4800ac99c92
emirhanozsoy <emirhan.ozsoy@gmail.com>
2019-02-21 21:36:30 +0100

    Add files via upload

---
+++ Data Communication Square Wave/Kare Dalga Proje/Kare Dalga Deneme 2/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Kare_Dalga_Deneme_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(356, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(144, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Sinüs Bileşenlerini Göster";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Çıkış";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(77, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Harmonik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Frekans";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Genlik";
+            // 
+            // chart1
+            // 
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(-63, 117);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1800, 552);
+            this.chart1.TabIndex = 24;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kare Dalga";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Data Communication Square Wave/Kare Dalga Proje/Kare Dalga Deneme 2/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Graph = System.Windows.Forms.DataVisualization.Charting;
+
+namespace Kare_Dalga_Deneme_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int genlik, frekans, harmonik;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.ChartAreas.Add("ChartArea1");
+            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = 4;
+            chart1.ChartAreas["ChartArea1"].AxisX.Interval = 0.5;
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineColor = Color.White;
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineDashStyle = Graph.ChartDashStyle.Dash;
+            chart1.ChartAreas["ChartArea1"].AxisY.Minimum = -1;
+            chart1.ChartAreas["ChartArea1"].AxisY.Maximum = 1;
+            chart1.ChartAreas["ChartArea1"].AxisY.Interval =0.5;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineColor = Color.White;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineDashStyle = Graph.ChartDashStyle.Dash;
+            chart1.ChartAreas["ChartArea1"].BackColor = Color.Black;
+           
+
+        }
+        double[] toplam =new double[1000];
+        double deger(int i, double x, int genlik, int frekans)
+        {
+            if (i == 1)
+            {
+                return 0;
+            }
+            else
+            {
+
+                return (4 * genlik / (i * Math.PI) * (Math.Sin(10 * i * 2 * frekans * x * Math.PI / 180))) + deger(i - 2, x, genlik, frekans);
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            genlik = int.Parse(textBox1.Text);
+            frekans = int.Parse(textBox2.Text);
+            harmonik = int.Parse(textBox3.Text);
+           
+
+            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = 72;
+            chart1.ChartAreas["ChartArea1"].AxisX.Interval =1;
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineColor = Color.White;
+            chart1.ChartAreas["ChartArea1"].AxisX.MajorGrid.LineDashStyle = Graph.ChartDashStyle.Dash;
+            chart1.ChartAreas["ChartArea1"].AxisY.Minimum = -genlik * 1.5;
+            chart1.ChartAreas["ChartArea1"].AxisY.Maximum = genlik*1.5;
+            chart1.ChartAreas["ChartArea1"].AxisY.Interval =1.5;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineColor = Color.White;
+            chart1.ChartAreas["ChartArea1"].AxisY.MajorGrid.LineDashStyle = Graph.ChartDashStyle.Dash;
+            chart1.Series.Clear();
+            int a = 1;
+            toplam[0] = 0.0;
+            chart1.Series.Add("Kare Dalga");
+            chart1.Series["Kare Dalga"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.Series["Kare Dalga"].Color = Color.Green;
+            chart1.Series["Kare Dalga"].BorderWidth = 5;
+                for (int i = 1; i <= harmonik; i=i+2)
+                {
+                    chart1.Series.Add("Harmonik"+i);
+                    chart1.Series["Harmonik" + i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                    for (double x = 0; x <= 72; x += 0.1)
+                    {
+                        if(checkBox1.Checked==true)
+                            chart1.Series["Harmonik" + i].Points.AddXY(x, 4 * genlik / (i * Math.PI) * (Math.Sin(10 * i * frekans * 2 * x * Math.PI / 180)));                                           
+                    }
+                }
+                for (int i = 1; i <= harmonik; i = i + 2)
+                {
+                    chart1.Series["Kare Dalga"].Points.Clear();
+                    for (double x = 0; x <= 72; x += 0.1)
+                    {
+                        chart1.Series["Kare Dalga"].Points.AddXY(x, (4 * genlik / (1 * Math.PI) * (Math.Sin(10 * 1 * frekans * 2 * x * Math.PI / 180))) + deger(i, x, genlik, frekans));
+                    }
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            for (int i = 1; i <= harmonik; i = i + 2)
+            {
+                for (double x = 0; x <= 36; x += 0.1)
+                {
+                    chart1.Series["Harmonik" + i].Points.Clear();
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
